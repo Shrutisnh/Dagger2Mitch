bbef4ceb11a7e77ae872d7008bf39b6829a75873
Shruti <shruti_sinha@persistent.com>
2021-02-26 12:01:18 +0530

    Application Component

---
+++ app/src/main/java/com/demo/dagger2/di/AppComponent.java
@@ -0,0 +1,22 @@
+package com.demo.dagger2.di;
+
+import android.app.Application;
+
+import com.demo.dagger2.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(modules = {AndroidSupportInjectionModule.class})
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+        AppComponent build();
+
+        @BindsInstance
+        Builder application(Application application);
+    }
+}
