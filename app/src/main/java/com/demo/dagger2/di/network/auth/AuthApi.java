68305a44a8fbb201573a4c8d78117a46e2616ef6
Shruti <shruti_sinha@persistent.com>
2021-03-03 12:42:28 +0530

    Retrofit calls and Rx Java

--- app/src/main/java/com/demo/dagger2/di/network/auth/AuthApi.java
+++ app/src/main/java/com/demo/dagger2/di/network/auth/AuthApi.java
@@ -1,10 +1,14 @@
 package com.demo.dagger2.di.network.auth;
 
+import com.demo.dagger2.models.User;
+
+import io.reactivex.Flowable;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface AuthApi {
-    @GET
-    Call<ResponseBody> getFakeStuffs();
+    @GET("users/{id}")
+    Flowable<User> getUser(@Path("id") int id);
 }
--- app/src/main/java/com/demo/dagger2/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/demo/dagger2/ui/auth/AuthViewModel.java
@@ -5,8 +5,14 @@ import android.util.Log;
 import androidx.lifecycle.ViewModel;
 
 import com.demo.dagger2.di.network.auth.AuthApi;
+import com.demo.dagger2.models.User;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
     String TAG = "AuthViewModel";
@@ -15,9 +21,31 @@ public class AuthViewModel extends ViewModel {
     @Inject
     public AuthViewModel(AuthApi authApi){
         this.authApi = authApi;
-        if(authApi==null)
-            Log.d(TAG, "AuthViewModel: Auth view model is NULL");
-        else
-            Log.d(TAG, "AuthViewModel: Auth view model is NOT null");
+
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull User user) {
+                        Log.d(TAG, "onNext: "+user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Log.d(TAG, "onError: "+e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
